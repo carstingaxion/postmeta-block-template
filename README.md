5393aa737ce8da6d6a14458ece24e357406816c0
carstenbach <mail@carsten-bach.de>
2023-01-14 22:27:46 +0100

    New init

--- README.md
+++ README.md
@@ -1,16 +1,46 @@
-# mkaz block template
+# A create-block template for a dynamic block using postmeta
 
-A basic block template with RichText comnponent to be used with the [`@wordpress/create-block`](/packages/create-block/README.md) script.
+Spin up a dynamic block to show specific postmeta as server-side-rendered block, using this template for the WordPress/create-block package.
+
+---
+
+A basic block template with RichText comnponent to be used with the [`@wordpress/create-block`](https://github.com/WordPress/gutenberg/tree/HEAD/packages/create-block/README.md) script.
 
 ## Usage
 
 This block template can be used by running the following command:
 
 ```bash
-npx  @wordpress/create-block --template mkaz-block-template
+npx  @wordpress/create-block --template @carstenbach/postmeta-block-template
 ```
 
+Replace all occurences of `POSTMETA` with the snake_cased name of your post_meta.
+
+
+## Structure
+
+Once the command has completed, the following structure will be created:
+
+```
+- build
+- languages
+- src
+--> index.js
+--> style.scss
+--> save.js
+--> edit
+---> index.js
+---> level-icon.js
+---> level-toolbar.js
+---> editor.scss
+block.json
+package.json
+package-lock.json
+{postmeta-block}.php
+readme.txt
+render.php
+```
 
 ## About
 
-See blog post for details: https://mkaz.blog/wordpress/make-your-own-create-block-templates/
+This template is based on https://github.com/mkaz/mkaz-block-template and its related blog post for details: https://mkaz.blog/wordpress/make-your-own-create-block-templates/
--- index.js
+++ index.js
@@ -5,28 +5,62 @@ const { join } = require("path");
 
 module.exports = {
 	defaultValues: {
-		slug: "richtext-block",
-		namespace: "mkaz",
-		author: "Marcus Kazmierczak",
+		slug: "postmeta-block",
+		namespace: "cbstdsys",
+		author: "Carsten Bach",
 		category: "text",
-		title: "RichText Block",
-		description: "A Gutenberg block template with a RichText field.",
+		title: "A dynamic block using postmeta",
+		description: "A Gutenberg block to show specific postmeta as server-side-rendered block.",
 		dashicon: "hammer",
 		attributes: {
-			content: {
-				type: "string",
-				source: "html",
-				selector: "div",
+			textAlign: {
+				type: "string"
 			},
+			level: {
+				type: "number",
+				default: 2
+			}
 		},
 		supports: {
+			align: true,
+			color: {
+				gradients: true,
+				background: true,
+				text: true,
+				link: true
+			},
+			spacing: {
+				margin: true
+			},
+			typography: {
+		        fontSize: true,
+		        lineHeight: true
+		    },
+	    	multiple: true,
 			html: false,
+		    reusable: false
+		},
+		domainPath: "/languages",
+		customBlockJSON: {
+			usesContext: [ 
+				"postType",
+				"postId"
+			],
+			textdomain: "{{textdomain}}",
+		},
+		customScripts: {
+			"i18n:pot": "wp i18n make-pot . --exclude=node_modules",
+			"i18n:json": "wp i18n make-json languages --pretty-print --no-purge",
+			"i18n:mapjson": "wp i18n make-json languages --pretty-print --no-purge '--use-map={ \"src/index.js\": \"build/index.js\", \"src/save.js\": \"build/index.js\", \"src/edit/index.js\": \"build/index.js\", \"src/block.json\": \"build/block.json\" }'"
 		},
 		npmDependencies: [
+			"classnames",
 			"@wordpress/block-editor",
 			"@wordpress/blocks",
 			"@wordpress/components",
+			"@wordpress/core-data",
 			"@wordpress/i18n",
+			"@wordpress/icons"
 		],
 	},
 	templatesPath: join(__dirname, "templates"),
